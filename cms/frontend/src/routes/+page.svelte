910cb520f95d478715f81b4d0e1c900687760ca6
Quinn Khuc <qtk23@drexel.edu>
2023-02-06 18:30:30 -0500

    added tailwind and idle page

--- cms/frontend/src/routes/+page.svelte
+++ cms/frontend/src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/quiz">
+	<div
+		style="background-image: url('https://res.cloudinary.com/drljdv2k2/image/upload/v1675464469/Westphal%20Touchscreen/westphal_feature_static_i6wewj.jpg')"
+		class=" h-[100vh] bg-center bg-cover"
+	>
+		<div
+			class="absolute top-[50%] right-0 flex bg-drexel-dark-blue py-2 px-4 text-white w-[80%] justify-around items-center"
+		>
+			<h1>Tap to Explore Westphal Majors</h1>
+			<img src="/svg/FingerClick.svg" alt="Finger Click Icon" />
+		</div>
+	</div>
+</a>
--- cms/frontend/tailwind.config.cjs
+++ cms/frontend/tailwind.config.cjs
@@ -1,8 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['"Futura Std"', ...defaultTheme.fontFamily.sans]
+			},
+			colors: {
+				'drexel-dark-blue': '#102A4C',
+				'drexel-yellow': '#FBC700',
+				'drexel-light-blue': '#006298',
+				'drexel-bright-blue': '#6CACE4',
+				'drexel-light-orange': '#FF8F1C',
+				'drexel-light-red': '#C14C31',
+				'drexel-light-green': '#B7BF10'
+			},
+			screens: {
+				custom: '500px'
+			}
+		}
+	}
+};
