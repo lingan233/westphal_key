45249c25cd74c91373aa98a1a920d84a9f279131
Quinn Khuc <qtk23@drexel.edu>
2023-02-06 21:05:54 -0500

    Fix typescript errors

--- cms/client/src/routes/+page.svelte
+++ cms/client/src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	import finger_click from '$lib/svg/FingerClick.svg';
+</script>
+
 <a href="/quiz">
 	<div
 		style="background-image: url('https://res.cloudinary.com/drljdv2k2/image/upload/v1675464469/Westphal%20Touchscreen/westphal_feature_static_i6wewj.jpg')"
@@ -7,7 +11,7 @@
 			class="absolute top-[50%] right-0 flex bg-drexel-dark-blue py-2 px-4 text-white w-[80%] justify-around items-center"
 		>
 			<h1>Tap to Explore Westphal Majors</h1>
-			<img src="/svg/FingerClick.svg" alt="Finger Click Icon" />
+			<img src={finger_click} alt="Finger Click Icon" />
 		</div>
 	</div>
 </a>
